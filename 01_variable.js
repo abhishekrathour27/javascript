d30bbc0dbf8d4d61a90f798d823dc85cc6d91dc1
abhishekrathour27 <abhishekrathour96081@gmail.com>
2025-01-05 02:50:46 +0530

    javascript

---
+++ 01_variable.js
@@ -0,0 +1,5 @@
+console.log("Varibles in Javascript");
+let a = 5;
+let b = 7;
+let c=a+b;
+console.log(c)
---
+++ 02_datatype.js
@@ -0,0 +1,19 @@
+console.log("Data types in javascript");
+//premetive datatype
+let a = null;//Null datatype
+let b = 234;//Number datatype
+let c = true;//either false bolean datatype
+let d = BigInt(222) + BigInt(111);//BigInt datatype
+let e = "abhi";//String data type
+let f = Symbol("this is symbol");//Symbol datatype
+let g = undefined;//Undefined data type
+console.log(a, b, c, d, f, g);
+//non-premetive datatype
+//Objects in javascript
+const item = {
+    "a": 1,
+    "b": true,
+    "c": 3,
+    "d": 4,
+}
+console.log(item["c"]);
