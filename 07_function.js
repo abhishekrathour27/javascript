7985bbd1f2ce68506e2d9bae36a89b72d956a538
abhishekrathour27 <abhishekrathour96081@gmail.com>
2025-01-09 05:20:08 +0530

    js

--- 07_function.js
+++ 07_function.js
@@ -1,6 +1,10 @@
 function add(x, y) {
     return (a + b)
 }
-let a = 5
+function sub(x,y){
+    return(a-b)
+}
+let a = 15
 let b = 8
 console.log("addition of " + a + " and " + b + " is " + add(a, b))
+console.log("subtraction of " + a + " and " + b + " is " + sub(a, b))
