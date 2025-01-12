c41e043cd70d3b34d645cec5817de7a490350c57
abhishekrathour27 <abhishekrathour96081@gmail.com>
2025-01-13 00:41:08 +0530

    js

---
+++ 08_conversion.js
@@ -0,0 +1,19 @@
+let n = 33
+console.log(typeof n)
+let score = "44aaa"
+console.log(typeof score)
+
+let valueInNum = Number(score)
+console.log(typeof valueInNum)
+console.log(valueInNum)
+
+//conversion result
+// "33" = 33
+// "33aaa" = NaN(not a number)
+// true = 1
+// undefined = undefined
+let num = 22
+let stringNum = String(num)
+console.log(stringNum);
+console.log(typeof stringNum);
+
